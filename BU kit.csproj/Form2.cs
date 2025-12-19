46d6b6a271047555984abe415a1c4af48a359b96
haideralibajwaofficial <haideralibajwaofficial@gmail.com>
2025-12-19 11:48:51 +0500

    Add files via upload

---
+++ BU kit.csproj/Form2.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BU_Kit
+{
+    public partial class adminVerification : Form
+    {
+        Form Parent;
+        public adminVerification(Form parent)
+        {
+            InitializeComponent();
+            Parent = parent;
+        }
+
+        private void adminVerification_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string pass = passwordTxt.Text;
+
+            if (!(pass == menu.get(menu.enroll, "password")) || string.IsNullOrEmpty(pass))
+            {
+                MessageBox.Show("Invalid Password");
+                return;
+            }
+            else
+            {
+                MessageBox.Show("Logged In");
+
+                adminDashboard admDashboard = new adminDashboard(Parent);
+                admDashboard.Show();
+                this.Hide();
+            }
+        }
+
+
+    }
+}
